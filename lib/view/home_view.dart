faeb9c63404c2e9d45b2f2b2c1c47e55fe57442a
badadarr <badar.maulana.techno@gmail.com>
2022-01-15 21:08:59 +0700

    update pub

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:getwidget/getwidget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -26,9 +25,10 @@ class HomeView extends StatelessWidget {
         actions: [
           Container(
             margin: const EdgeInsets.only(right: 15),
-            child: GFAvatar(
-              backgroundImage: NetworkImage(
-                  "https://img.search.brave.com/jV1iTeZApLOfvedgttonPVxR9eIc2QAPN__OCdJ2mco/fit/474/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5F/ZElpLXZDOFpoSU1w/TmVWRFJTc3hnSGFI/YSZwaWQ9QXBp"),
+            child: CircleAvatar(
+              backgroundImage: AssetImage(
+                "assets/images/logo.png",
+              ),
             ),
           ),
         ],
